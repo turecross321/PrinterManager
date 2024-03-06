91946e89131a5fa5a27a3f2cfdbc191c540520f5
turecross321 <turesod@gmail.com>
2024-03-06 21:05:55 +0100

    Include migrations

---
+++ Migrations/20240306183603_Initial.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrinterManagerServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PrinterModels",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    PrintVolumeWidthMm = table.Column<float>(type: "REAL", nullable: false),
+                    PrintVolumeHeightMm = table.Column<float>(type: "REAL", nullable: false),
+                    PrintVolumeLengthMm = table.Column<float>(type: "REAL", nullable: false),
+                    ImageUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    ReleaseDate = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrinterModels", x => x.Guid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Guid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Printers",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false),
+                    ModelGuid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Active = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PurchaseDate = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Printers", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_Printers_PrinterModels_ModelGuid",
+                        column: x => x.ModelGuid,
+                        principalTable: "PrinterModels",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserGuid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreationDate = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    ExpiryDate = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    TokenType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Users_UserGuid",
+                        column: x => x.UserGuid,
+                        principalTable: "Users",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Printers_ModelGuid",
+                table: "Printers",
+                column: "ModelGuid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_UserGuid",
+                table: "Tokens",
+                column: "UserGuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Printers");
+
+            migrationBuilder.DropTable(
+                name: "Tokens");
+
+            migrationBuilder.DropTable(
+                name: "PrinterModels");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Migrations/20240306194924_AddPrinterReports.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrinterManagerServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrinterReports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Active",
+                table: "Printers",
+                newName: "Disabled");
+
+            migrationBuilder.CreateTable(
+                name: "PrinterReports",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PrinterGuid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Severity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    CreationDate = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Resolved = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ResolveDate = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    ResolveComment = table.Column<string>(type: "TEXT", nullable: true),
+                    ResolverGuid = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrinterReports", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_PrinterReports_Printers_PrinterGuid",
+                        column: x => x.PrinterGuid,
+                        principalTable: "Printers",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PrinterReports_Users_ResolverGuid",
+                        column: x => x.ResolverGuid,
+                        principalTable: "Users",
+                        principalColumn: "Guid");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrinterReports_PrinterGuid",
+                table: "PrinterReports",
+                column: "PrinterGuid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrinterReports_ResolverGuid",
+                table: "PrinterReports",
+                column: "ResolverGuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrinterReports");
+
+            migrationBuilder.RenameColumn(
+                name: "Disabled",
+                table: "Printers",
+                newName: "Active");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
